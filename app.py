431d685d928d5c2fc9abc72f5a88d962c8e4f144
EraldoJair <Eraldovaldiviapinto@gmail.com>
2023-07-21 11:04:30 -0500

    prueba con puertos por defecto

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def recibir_mensajes():
 if __name__ == "__main__":
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=5000)
-    app.run(host="0.0.0.0", port=5000)
-    # app.run()
+    # app.run(host="0.0.0.0", port=5000)
+    app.run()
